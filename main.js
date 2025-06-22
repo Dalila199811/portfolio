0a491b073a634df7656289145989b80274753810
Dalila199811 <dalila.negri3@gmail.com>
2025-06-22 22:38:02 +0200

    hi

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-// Loader animation: sun rays loading
-function createLoaderRays(numRays = 24, radius = 120, color = "#f56900", id = "loader-loading") {
+function createLoaderRays(numRays = 24, radius = 120, color = "#f56900", id = "loader-loading", innerRadius = 40) {
   const container = document.getElementById(id);
   if (!container) return;
   const size = radius * 2;
   let svg = `<svg width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" style="position:absolute;top:0;left:0;">`;
   for (let i = 0; i < numRays; i++) {
     const angle = (i / numRays) * 2 * Math.PI;
+    const x1 = radius + innerRadius * Math.cos(angle);
+    const y1 = radius + innerRadius * Math.sin(angle);
     const x2 = radius + radius * Math.cos(angle);
     const y2 = radius + radius * Math.sin(angle);
-    svg += `<line id="ray-${i}" x1="${radius}" y1="${radius}" x2="${x2}" y2="${y2}" stroke="${color}" stroke-width="3" opacity="0"/>`;
+    svg += `<line id="ray-${i}" x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" stroke="${color}" stroke-width="3" opacity="0"/>`;
   }
   svg += "</svg>";
   container.innerHTML = svg;
 }
 
-createLoaderRays(24, 120, "#f56900", "loader-loading");
+// spazio centrale
+createLoaderRays(30, 120, "#f56900", "loader-loading", 70);
 
-function animateLoaderRays(numRays = 24, duration = 3000) {
+function animateLoaderRays(numRays = 50, duration = 3000) {
   let current = 0;
   const interval = duration / numRays;
   const showRay = () => {
@@ -30,7 +32,7 @@ function animateLoaderRays(numRays = 24, duration = 3000) {
   showRay();
 }
 
-animateLoaderRays(24, 2000);
+animateLoaderRays(50, 3000);
 
 // Nascondi loader dopo 3 secondi
 window.addEventListener('load', () => {
@@ -38,8 +40,8 @@ window.addEventListener('load', () => {
     document.getElementById('loader-overlay').classList.add('hide');
     setTimeout(() => {
       document.getElementById('loader-overlay').style.display = 'none';
-    }, 700); // tempo per la dissolvenza CSS
-  }, 3000); // loader visibile per 3 secondi
+    }, 500); 
+  }, 3000); 
 });
 
 
@@ -134,7 +136,7 @@ gsap.to("#elemento2-gsap", {
 });
 
 
-
+//  marquee
 let currentScroll = 0;
 let isScrollingDown = true;
 let arrows = document.querySelectorAll('.arrow');
@@ -172,7 +174,7 @@ currentScroll = window.pageYOffset;
 });
 
 gsap.to("#marquee-gsap", {
-  x: "-100%",
+  x: 100,
   duration: 25, // più alto = più lento
   ease: "linear",
   repeat: -1
@@ -184,3 +186,4 @@ const navLinks = document.getElementById('nav-links');
 toggle.addEventListener('click', () => {
   navLinks.classList.toggle('open');
 });
+
