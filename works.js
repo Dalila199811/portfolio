dabd2f37adfaa55f09250046f7f4d45394b5f778
Dalila199811 <dalila.negri3@gmail.com>
2025-06-23 20:59:48 +0200

    ok

--- works.js
+++ works.js
@@ -1,34 +1,32 @@
-const cursor = document.querySelector(".custom-cursor");
-const cursorImage = document.getElementById("cursor-image");
-const listItems = document.querySelectorAll(".work-list li");
 
-// Segui il mouse con GSAP
-document.addEventListener("mousemove", (e) => {
-  gsap.to(cursor, {
-    duration: 0.3,
-    x: e.clientX,
-    y: e.clientY,
-    ease: "power2.out"
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('.fade').forEach(section => {
+      gsap.fromTo(section, 
+        { opacity: 0, y: 50 }, 
+        {
+          opacity: 1, y: 0, duration: 1,
+          scrollTrigger: {
+            trigger: section,
+            start: "top 80%",
+            toggleActions: "play none none reverse"
+          }
+        }
+      );
+    });
+
+const sections = document.querySelectorAll('.fade');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
   });
+}, {
+  threshold: 0.3
 });
 
-// Mostra l'immagine quando entri in un <li>
-listItems.forEach(item => {
-  item.addEventListener("mouseenter", () => {
-    const image = item.getAttribute("data-image");
-    cursorImage.src = image;
-    gsap.to(cursor, {
-      autoAlpha: 1,
-      scale: 1,
-      duration: 0.3
-    });
-  });
-
-  item.addEventListener("mouseleave", () => {
-    gsap.to(cursor, {
-      autoAlpha: 0,
-      scale: 0.8,
-      duration: 0.2
-    });
-  });
+sections.forEach(section => {
+  observer.observe(section);
 });
